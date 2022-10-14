909842be5eebb226b9e1f1c1bfa39901ac4113cb
Dfdragon12 <61027677+Dfdragon12@users.noreply.github.com>
2022-10-14 09:21:54 -0500

    Add files via upload

---
+++ src/main/java/co/edu/unisabana/usuario/controller/BookController.java
@@ -0,0 +1,43 @@
+package co.edu.unisabana.usuario.controller;
+
+import co.edu.unisabana.usuario.dto.BookDto;
+import co.edu.unisabana.usuario.dto.BookReponse;
+import co.edu.unisabana.usuario.repository.dao.BookDao;
+import co.edu.unisabana.usuario.repository.dao.entity.BookEntity;
+import co.edu.unisabana.usuario.service.library.RegisterBookLibrary;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/book")
+public class BookController {
+
+    private final RegisterBookLibrary registerBookLibrary;
+
+
+    public BookController(RegisterBookLibrary registerBookLibrary) {
+        this.registerBookLibrary = registerBookLibrary;
+    }
+
+    // Pendiente validar uso de exception handler
+    // explicar camel case
+    @PostMapping("/register")
+    @ResponseBody
+    public BookReponse registerBook(@RequestBody BookDto bookDto) {
+        int result = registerBookLibrary.registerBook(bookDto.toModel());
+        if (result == 1) {
+            return new BookReponse("Actualizada cantidad");
+        }
+        return new BookReponse("Nuevo libro registrado");
+
+
+    }
+
+    @GetMapping("/search")
+    public List<BookEntity> searchBook(@RequestParam String q) {
+        BookDao bookDao = new BookDao();
+        List<BookEntity> results = bookDao.searchBook(q);
+        return results;
+    }
+}
